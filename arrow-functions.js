3dc5108c069115b700c28a7931a6307244a99738
Luke Masterson <lukemasterson@Lukes-MacBook-Air.local>
2024-03-13 21:11:55 +0000

    Added arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,16 +4,26 @@
  */
 
 // Vanilla JavaScript Function
-function addTwoNumbers(a, b) {
+// function addTwoNumbers(a, b) {
+//     // Code Block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
+
+// Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
     // Code Block
     return a + b;
 }
 let sum = addTwoNumbers(3, 5);
 console.log(sum);
 
-// Arrow Function With Parameters
-
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
 
